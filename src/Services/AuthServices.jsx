1ea399ee0030309a09835427318f6fcacb40dc15
createaniket <aniketpratapsingh7310@gmail.com>
2024-06-24 18:59:36 +0530

    Server fixed now

--- src/Services/AuthServices.jsx
+++ src/Services/AuthServices.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://session-backend-0aoq.onrender.com';
+const API_URL = 'https://session-backend-0aoq.onrender.com/api/auth/';
 
 const signup = async (email, password) => {
     const response = await axios.post(`${API_URL}/signup`, { email, password });
