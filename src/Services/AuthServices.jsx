ad29ed1d867b4bd75d7ee50c440e1264655d1b69
createaniket <aniketpratapsingh7310@gmail.com>
2024-06-25 13:22:07 +0530

    some glitch fixed

--- src/Services/AuthServices.jsx
+++ src/Services/AuthServices.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://session-backend-0aoq.onrender.com/api/auth/';
-const API_URL = 'http://localhost:9000/api/auth/';
+const API_URL = 'https://session-backend-0aoq.onrender.com/api/auth/';
+// const API_URL = 'http://localhost:9000/api/auth/';
 const signup = async (email, password) => {
     const response = await axios.post(`${API_URL}/signup`, { email, password });
     return response.data;
